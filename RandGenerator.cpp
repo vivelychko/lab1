b274e81fbf16b9b7edbb33432b3cc9eabe062afe
Viktoriia Velychko <146025392+vivelychko@users.noreply.github.com>
2023-09-28 21:45:31 +0300

    Added Polar Coordinate Method

--- RandGenerator.cpp
+++ RandGenerator.cpp
@@ -11,11 +11,7 @@ private:
     int m = 19997;
     int x = 3;
     int n = 1000;
-
     vector<double> v_values;
-    vector<int> v_histogram;
-    vector<int> vector1;
-    vector<int> vector2;
 
 public:
     void LinearCongruentialMethod() {
@@ -44,13 +40,14 @@ public:
         while (v_values.size() < maxDigits) {
             int nextFib = a + b;
             v_values.push_back(nextFib);
+
             a = b;
             b = nextFib;
         }
     }
 
     int find_module_inversed(int x, int m){
-        int a = 1; // починаємо з одиниці
+        int a = 1;
         while ((a * x) % m != 1){ // перевіряємо чи a * x залишок від ділення m дорівнює 1, якщо не дорівнює то перемо наступне число, а якщо дорівнює то а і є оберненим до x за модулем m
             a++; // переходимо на наступне число
         }
@@ -60,9 +57,14 @@ public:
     void InverseCongruentialGenerator() {
         for (int i = 0; i < n; ++i) {
             int inver_x = find_module_inversed(x, m);
-            x = (a *  inver_x + b) % m;
-            v_values.push_back((float) x / m);
+            x = (a * inver_x + b) % m;
+            float u = (float) x / m;
+            v_values.push_back(u);
         }
+    }
+
+    void CombiningMethod() {
+
     }
 
     void ThreeSigmaMethod() {
@@ -81,12 +83,34 @@ public:
         }
     }
 
+    void PolarCoordinateMethod() {
+        for (int i = 0; i < n; ++i) {
+            double U1 = 0, U2 = 0, V1 = 0, V2 = 0, S = 0;
+
+            do {
+                U1 = 2.0 * rand() / RAND_MAX - 1.0;
+                U2 = 2.0 * rand() / RAND_MAX - 1.0;
+                S = U1 * U1 + U2 * U2;
+            } while (S >= 1.0 || S == 0.0);
+
+            V1 = U1 * sqrt(-2.0 * log(S) / S);
+            V2 = U2 * sqrt(-2.0 * log(S) / S);
+
+            v_values.push_back(V1);
+            v_values.push_back(V2);
+        }
+    }
+
+    void RatioMethod() {
+
+    }
+
     void showHistogram0To1() {
         cout << "Interval\tFrequency" << endl;
         int c1 = 0, c2 = 0, c3 = 0, c4 = 0, c5 = 0, c6 = 0, c7 = 0, c8 = 0, c9 = 0, c10 = 0;
 
         for (int i = 0; i < n; ++i) {
-                if (v_values[i] < 0.1) { c1++; }
+                if (v_values[i] < 0.1)  { c1++; }
             else if (v_values[i] < 0.2) { c2++; }
             else if (v_values[i] < 0.3) { c3++; }
             else if (v_values[i] < 0.4) { c4++; }
@@ -127,7 +151,7 @@ public:
             else c10++;
         }
 
-        cout << "[-3 to -2.4]\t" << (float) c1/n << endl;
+        cout << "[-3 to -2.4]\t" <<   (float) c1/n << endl;
         cout << "[-2.4 to -1.8]\t" << (float) c2/n << endl;
         cout << "[-1.8 to -1.2]\t" << (float) c3/n << endl;
         cout << "[-1.2 to -0.6]\t" << (float) c4/n << endl;
@@ -146,7 +170,7 @@ public:
             cout << "[2] Quadratic congruential method" << endl;
             cout << "[3] Digit of Fibonacci" << endl;
             cout << "[4] Inverse congruential generator" << endl;
-            cout << "[5] Method of merge" << endl;
+            cout << "[5] Combining Method" << endl;
             cout << "[6] 3-x sigma" << endl;
             cout << "[7] Polar coordinate method" << endl;
             cout << "[8] The ratio method" << endl;
@@ -158,9 +182,11 @@ public:
 
             switch (choice) {
                 case 0: return;
+
                 case 1:
                     LinearCongruentialMethod();
                     showHistogram0To1();
+                    showHistogramNeg3To3();
                     break;
 
                 case 2:
@@ -179,11 +205,22 @@ public:
                     break;
 
                 case 5:
-
+                    CombiningMethod();
+                    showHistogram0To1();
                     break;
 
                 case 6:
                     ThreeSigmaMethod();
+                    showHistogramNeg3To3();
+                    break;
+
+                case 7:
+                    PolarCoordinateMethod();
+                    showHistogramNeg3To3();
+                    break;
+
+                case 8:
+                    RatioMethod();
                     showHistogramNeg3To3();
                     break;
 
@@ -193,8 +230,7 @@ public:
             }
 
             v_values.clear();
-            v_histogram.clear();
-            choice = 0;
+
             cout << endl;
         } while (true);
     }
@@ -203,5 +239,4 @@ public:
 int main() {
     Generators generators;
     generators.showMenu();
-    return 0;
 }
